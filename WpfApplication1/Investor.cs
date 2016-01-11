cfe2a638ef563c867c6d631c3212796415b7fdd0
medion89 <medion89@gmail.com>
2016-01-11 15:26:33 +0200

    123
    new

---
+++ WpfApplication1/Investor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+namespace WpfApplication1
+{
+   public class Investor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float Summ { get; set; }
+        public string City { get; set; }
+
+        private ObservableCollection<Investor> _investorsList = null;
+        public ObservableCollection<Investor> InvestorsList
+        {
+            get
+            {
+                if(_investorsList!= null)
+                {
+                    return _investorsList;
+                }
+                _investorsList = new ObservableCollection<Investor>();
+                _investorsList.Add(new Investor()
+                {
+                    FirstName = "Jhon",
+                    LastName = "Doe",
+                    Summ = 100000,
+                    City = "Budapesht"
+                });
+                return _investorsList;
+            }
+
+        }
+
+    }
+}
